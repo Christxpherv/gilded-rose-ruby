a932d7d69ebf1db07454c1a1bfe4fb94172c17aa
Christopher Valle <christxpherv@gmail.com>
2024-02-29 22:12:37 -0600

    step six: made classes for every previous method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -23,6 +23,52 @@ class GildedRose
     end
   end
 
+  class Brie
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
+  class Sulfuras
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+
+    end
+  end
+
+  class Backstage
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return              if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
+  end
+
   def tick
     # create case statement to call the correct method
     case name
@@ -45,26 +91,20 @@ class GildedRose
 
   # added brie_tick method
   def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
+    @item = Brie.new(@quality, @days_remaining)
+    item.tick
   end
 
   # make sulfuras_tick method
   def sulfuras_tick
+    @item = Sulfuras.new(@quality, @days_remaining)
+    item.tick
   end
 
   # make backstage_tick method
   def backstage_tick
-    @days_remaining -= 1
-    return              if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+    @item = Backstage.new(@quality, @days_remaining)
+    item.tick
   end
 
   def quality
