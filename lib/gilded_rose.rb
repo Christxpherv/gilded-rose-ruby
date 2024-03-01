cbfd6419016a462cca99fe0050ab2144ebf971e3
Christopher Valle <christxpherv@gmail.com>
2024-02-29 20:17:26 -0600

    step four: refactor normal_tick method and create various methods accessed using case statement in tick method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,9 +6,19 @@ class GildedRose
     @quality = quality
   end
   def tick
-    if name == "Normal Item"
+    # create case statement to call the correct method
+    case name
+    when "Normal Item"
       return normal_tick
+    when "Aged Brie"
+      return brie_tick
+    when "Sulfuras, Hand of Ragnaros"
+      return sulfuras_tick
+    when "Backstage passes to a TAFKAL80ETC concert"
+      return backstage_tick
     end
+
+
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         if @name != "Sulfuras, Hand of Ragnaros"
@@ -56,15 +66,36 @@ class GildedRose
     end
   end
 
+  # refactor the normal_tick method more
   def normal_tick
-    if @quality != 0
-      if @days_remaining > 0
-        @quality -= 1
-      end
-      if @days_remaining <= 0
-        @quality -= 2
-      end
-    end
     @days_remaining -= 1
+    return if @quality == 0
+
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
+  end
+
+  # added brie_tick method
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0 && @quality < 50
+  end
+
+  # make sulfuras_tick method
+  def sulfuras_tick
+  end
+
+  # make backstage_tick method
+  def backstage_tick
+    @days_remaining -= 1
+    return              if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
   end
 end
