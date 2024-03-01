2e82096ec56487f4ea75e0d7772270f0a2f266e1
Christopher Valle <christxpherv@gmail.com>
2024-02-29 18:32:51 -0600

    step three: get to green with normal_tick method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,17 +1,13 @@
 class GildedRose
   attr_reader :name, :days_remaining, :quality
-
   def initialize(name:, days_remaining:, quality:)
     @name = name
     @days_remaining = days_remaining
     @quality = quality
   end
-
   def tick
-
     if name == "Normal Item"
       return normal_tick
-
     end
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
@@ -37,11 +33,6 @@ class GildedRose
       end
     end
 
-    def normal_tick
-
-    end
-
-
 
     if @name != "Sulfuras, Hand of Ragnaros"
       @days_remaining = @days_remaining - 1
@@ -63,5 +54,17 @@ class GildedRose
         end
       end
     end
+  end
+
+  def normal_tick
+    if @quality != 0
+      if @days_remaining > 0
+        @quality -= 1
+      end
+      if @days_remaining <= 0
+        @quality -= 2
+      end
+    end
+    @days_remaining -= 1
   end
 end
